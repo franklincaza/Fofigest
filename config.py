60c7eeba34f4ac1cc8aabdc4e0f8a05fc8a6bb63
FranklinOfimatic <86037750+franklincaza@users.noreply.github.com>
2024-09-30 11:28:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 config={
-    "debug":True,
-    "contraseña_google":"qkyh jfuw tixt fkcs",
-    "EMAIL":"fofimaticsas@gmail.com",
-    "host":"http://127.0.0.1:5000/"
+    "debug":False,
+    "contraseña_google":"",
+    "EMAIL":"",
+    "host":""
 
 }
 
-# Dv@4VdaG@J3W*Qu
-# anonpublic : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxYm5wZnJnc3hmdXVqYm52a2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0MDEzNDgsImV4cCI6MjA0Mjk3NzM0OH0.XC63zW8T0VINcrjz-jZsLpiQtnZ5zzJeMFCkwbT3ekk
- 
